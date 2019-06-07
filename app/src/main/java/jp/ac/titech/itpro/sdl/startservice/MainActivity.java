e54c01c56a8a7917b1d7751c3bf011486ca57b9d
linsho <kaku.l.aa@rio.gsic.titech.ac.jp>
2019-06-07 17:17:02 +0900

    課題の実装

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,18 +1,34 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    private IntentFilter intentFilter;
+    private BroadcastReceiver broadcastReceiver;
+    public final static String EXTRA_MYARG = "MAIN_MYARG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Toast toast = Toast.makeText(context, "受け取ります:"+intent.getStringExtra(EXTRA_MYARG), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        };
+        intentFilter = new IntentFilter("SERVICE_3");
+        registerReceiver(broadcastReceiver, intentFilter);
         setContentView(R.layout.activity_main);
     }
 
@@ -25,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickTest2(View v) {
         Log.d(TAG, "onClickTest2 in " + Thread.currentThread());
-        Intent intent = new Intent(this, Service2.class);
-        intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
+        Intent intent = new Intent(this, Service3.class);
+        intent.putExtra(Service3.EXTRA_MYARG, "Hello, Service3");
         startService(intent);
     }
 }
